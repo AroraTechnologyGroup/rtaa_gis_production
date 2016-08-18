de0de21dcc2d19c3246d89398da419540b73f33f
Richard Hughes <richardh522@gmail.com>
2016-08-17 23:58:10 -0400

    fixing signin

--- rtaa_gis/home/urls.py
+++ rtaa_gis/home/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import include, url
-from django.contrib.auth.views import logout_then_login, login, password_change
+from django.contrib.auth.views import logout, login, password_change
 from .views import HomePage
 
 app_name = 'home'
 urlpatterns = [
-    url(r'^$', HomePage.process),
-    url(r'login/$', login, name='login'),
-    url(r'logout/$', logout_then_login, name='logout'),
-    url(r'password_change/$', password_change, {'next': '/'}, name='password_change')
+    url(r'^$', HomePage.as_view(), name='index'),
+    url(r'^login/$', login, name='login'),
+    url(r'^logout/$', logout, {'next_page': 'home:login'}, name='logout'),
+    url(r'^password_change/$', password_change, {'next': '/'}, name='password_change')
 ]
 
 
--- rtaa_gis/home/views.py
+++ rtaa_gis/home/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from django.shortcuts import redirect
 from django.contrib.auth import authenticate, login, views
+from django.urls import reverse
 
 
 # Create your views here.
@@ -13,20 +14,20 @@ class HomePage(APIView):
     permission_classes = (AllowAny,)
     template = r'home/main_content.html'
 
-    def process(self, request):
-        if request.POST:
+    def post(self, request):
+        if not request.user.is_authenticated():
             username = request.POST["username"]
             password = request.POST["password"]
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
-                return redirect('home:login')
+                return redirect(reverse('home:process'))
 
-        elif request.GET:
-            if not request.user.is_authenticated():
-                return redirect('login/', next='')
-            context = {"data": []}
-            return Response(context, template_name=self.template)
+    def get(self, request):
+        if not request.user.is_authenticated():
+            return redirect(reverse('home:login'), next='')
+        context = {"data": []}
+        return Response(context, template_name=self.template)
 
 
 
