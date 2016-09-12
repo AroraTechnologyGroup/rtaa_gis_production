6b272051736bade49effa812f67a39f8d68b2b9b
Richard Hughes <richardh522@gmail.com>
2016-09-11 21:03:08 -0400

    update layout of homepage and login page

--- rtaa_gis/utils/buildDocStore.py
+++ rtaa_gis/utils/buildDocStore.py
@@ -8,7 +8,7 @@ import json
 import traceback
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'eDocSearchAPI.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'rtaa_gis.settings'
 django.setup()
 from fileApp import models
 
