8345bd709a489dd1b344e5845b0ad9080f633af4
Richard Hughes <richardh522@gmail.com>
2016-09-21 09:05:48 -0400

    update template dir and deploy

--- rtaa_gis/utils/deploy.py
+++ rtaa_gis/utils/deploy.py
@@ -1,3 +1,4 @@
+from rtaa_gis.settings import BASE_DIR
 import cgitb
 import subprocess
 from subprocess import PIPE
@@ -16,11 +17,21 @@ def pull(path):
     proc = subprocess.Popen(git_cmd, **kwargs)
     (std_out, std_err) = proc.communicate()
 
+    kwargs = dict()
+    kwargs['cwd'] = BASE_DIR
+    kwargs['stderr'] = PIPE
+    kwargs['stdout'] = PIPE
+    kwargs['universal_newlines'] = True
+    python_path = r"venv\scripts\python.exe"
+    manage_script = "manage.py"
+    proc = subprocess.Popen("{} {} collectstatic --no-input".format(python_path, manage_script), **kwargs)
+    (out, err) = proc.communicate()
+
     connection = mail.get_connection()
     connection.open()
     send_mail(
         "Deploy RTAA_GIS to Staging",
-        "std_out: {} \n std_err: {}".format(std_out, std_err),
+        "std_out: {}\n std_err: {}\n collectstatic: {}\n err: {}".format(std_out, std_err, out, err),
         "rhughes@aroraengineers.com",
         ["richardh522@gmail.com"],
         fail_silently=False,
