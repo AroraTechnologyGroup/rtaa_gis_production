4d7dcfbeeff1c1d74a89fdc746b21f9023003e77
Richard Hughes <richardh522@gmail.com>
2016-10-05 17:10:22 -0400

    setting database in test settings

--- rtaa_gis/tests/test_settings.py
+++ rtaa_gis/tests/test_settings.py
@@ -1,4 +1,14 @@
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 SECRET_KEY = 'fake-key'
 INSTALLED_APPS = [
     'tests'
 ]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'test.sqlite3'),
+    }
+}
