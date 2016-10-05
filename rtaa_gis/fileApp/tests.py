d1e848a2834e9d91d6596133ebfd47caf1cf75c3
Richard Hughes <richardh522@gmail.com>
2016-10-04 22:44:05 -0400

    update settings

--- rtaa_gis/fileApp/tests.py
+++ rtaa_gis/fileApp/tests.py
@@ -2,11 +2,11 @@ from django.test import TestCase
 import logging
 import sys
 import os
-from eDocSearchAPI import settings
+from rtaa_gis import settings
 import django
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'eDocSearchAPI.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'rtaa_gis.settings'
 django.setup()
 
 # Create your tests here.
