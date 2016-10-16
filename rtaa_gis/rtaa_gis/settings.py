727ddb3953f541e2182f0b05d1e3e783bd5bdc51
Richard Hughes <richardh522@gmail.com>
2016-10-16 12:16:40 -0400

    added ldap python3 app for testing against IIS, Roll back to the commit prior to this one if shit breaks

--- rtaa_gis/rtaa_gis/settings.py
+++ rtaa_gis/rtaa_gis/settings.py
@@ -76,6 +76,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_python3_ldap',
     'django_extensions',
     'rest_framework_swagger',
     'crispy_forms',
@@ -87,7 +88,7 @@ INSTALLED_APPS = [
 ]
 
 AUTHENTICATION_BACKENDS = (
-    'django.contrib.auth.backends.RemoteUserBackend',
+    "django_python3_ldap.auth.LDAPBackend",
     'django.contrib.auth.backends.ModelBackend',
 )
 
@@ -257,3 +258,8 @@ USE_TZ = True
 ROOT_URLCONF = r'rtaa_gis.urls'
 LOGIN_URL = r'login/'
 LOGIN_REDIRECT_URL = r'/'
+
+# The LDAP search base for looking up users.
+LDAP_AUTH_SEARCH_BASE = "ou=gisapps,dc=aroraengineers,dc=com"
+LDAP_AUTH_FORMAT_USERNAME = "django_python3_ldap.utils.format_username_active_directory"
+LDAP_AUTH_ACTIVE_DIRECTORY_DOMAIN = "GISAPPS"
