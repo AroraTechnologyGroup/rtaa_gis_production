04b2c8704520c54543800fe59c2bde5e1f27b9c3
Richard Hughes <richardh522@gmail.com>
2016-11-29 19:04:21 -0500

    adding get groups view

--- rtaa_gis/home/urls.py
+++ rtaa_gis/home/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import url
 from django.contrib.auth.views import logout, login, password_change
-from .views import HomePage
+from .views import HomePage, user_groups
+
 from rest_framework.authtoken import views
 
 
 app_name = 'home'
 urlpatterns = [
     url(r'^$', HomePage.as_view(), name='index'),
+    url(r'^groups/$', user_groups),
     url(r'^login/$', login, {'extra_context': {'next': '/#home'}}, name='login'),
     url(r'^logout/$', logout, {'next_page': 'home:login'}, name='logout'),
     url(r'^dojo-login/$', views.obtain_auth_token, name='dojo-login'),
