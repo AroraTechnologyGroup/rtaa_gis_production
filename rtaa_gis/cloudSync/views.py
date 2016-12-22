0d4f4426265abcb06f9a23c9dbe7448160bed414
Richard Hughes <richardh522@gmail.com>
2016-12-22 11:29:03 -0500

    using Conda in TravisCI

--- rtaa_gis/cloudSync/views.py
+++ rtaa_gis/cloudSync/views.py
@@ -1,6 +1,5 @@
 import os
 from subprocess import PIPE
-from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer, TemplateHTMLRenderer
