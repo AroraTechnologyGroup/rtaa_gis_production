ad2ff5fd9433161b89fdcad1c8e3081c04cad7c1
Richard Hughes <richardh522@gmail.com>
2017-01-03 16:34:49 -0500

    repair RemoteUser username bug

--- rtaa_gis/rtaa_gis/settings.py
+++ rtaa_gis/rtaa_gis/settings.py
@@ -40,14 +40,14 @@ EMAIL_HOST = "aspmx.l.google.com"
 SECRET_KEY = 'bo0*s)^co9abj49*kpp(+91&98v25=0s3#3bv-3-l(2hg9q!5c'
 
 SECURE_SSL_REDIRECT = False
-SESSION_COOKIE_SECURE = False
+SESSION_COOKIE_SECURE = True
 # Leave these commented out for testing
-# CSRF_TRUSTED_ORIGINS = ["localhost", "127.0.0.1:8080", "gisapps.aroraengineers.com"]
+# CSRF_TRUSTED_ORIGINS = ["localhost", "gisapps.aroraengineers.com"]
 # CSRF_COOKIE_DOMAIN = ['.aroraengineers.com']
-CSRF_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = True
 CORS_ALLOW_CREDENTIALS = True
-CORS_REPLACE_HTTPS_REFERER = False
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_REPLACE_HTTPS_REFERER = True
+CORS_ORIGIN_ALLOW_ALL = False
 
 CORS_ALLOW_HEADERS = (
     'x-requested-with',
