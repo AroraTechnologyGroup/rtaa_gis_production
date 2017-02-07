66d20b2cca165d51fa0c386323ae806b05832d43
Richard Hughes <richardh522@gmail.com>
2017-02-07 15:19:26 -0500

    successfull printing with authentication

--- rtaa_gis/rtaa_gis/settings.py
+++ rtaa_gis/rtaa_gis/settings.py
@@ -17,7 +17,7 @@ from django.urls import reverse
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 DATA_UPLOAD_MAX_MEMORY_SIZE = 26214400
-APPEND_SLASH = True
+
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
@@ -44,15 +44,15 @@ SECRET_KEY = 'bo0*s)^co9abj49*kpp(+91&98v25=0s3#3bv-3-l(2hg9q!5c'
 SECURE_SSL_REDIRECT = False
 SESSION_COOKIE_SECURE = False
 
-CSRF_TRUSTED_ORIGINS = ('gisapps.aroraengineers.com:8004', 'https://localhost:3003',
-                        'https://gisapps.aroraengineers.com:3344')
+CSRF_TRUSTED_ORIGINS = ('gisapps.aroraengineers.com:8004', 'localhost:3003',
+                        'gisapps.aroraengineers.com:3344', 'gisapps.aroraengineers.com')
 # CSRF_COOKIE_DOMAIN = ['.aroraengineers.com']
 CSRF_COOKIE_SECURE = False
 CORS_ALLOW_CREDENTIALS = True
 CORS_REPLACE_HTTPS_REFERRER = True
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = (
-    'gisapps.aroraengineers.com',
+    'https://gisapps.aroraengineers.com',
     'localhost:3003',
     'localhost:3001',
     'gisapps.aroraengineers.com:8004',
@@ -80,13 +80,13 @@ ALLOWED_HOSTS = [
 ]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 if not DEBUG:
-    SECURE_SSL_REDIRECT = True
+    SECURE_SSL_REDIRECT = False
     SESSION_COOKIE_SECURE = False
     CSRF_COOKIE_SECURE = False
-    CORS_REPLACE_HTTPS_REFERER = False
+    CORS_REPLACE_HTTPS_REFERER = True
     CORS_ORIGIN_ALLOW_ALL = False
 
 INSTALLED_APPS = [
