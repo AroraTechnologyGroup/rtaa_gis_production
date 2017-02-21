62a870eb64565cd05a81c08e56769cb8b3f439c4
Richard Hughes <richardh522@gmail.com>
2017-02-20 21:52:17 -0500

    removing creds from source

--- rtaa_gis/cloudSync/utils/SearchTool.py
+++ rtaa_gis/cloudSync/utils/SearchTool.py
@@ -1,6 +1,10 @@
 from arcgis.gis import GIS
 import json
+import os
 
+cred = open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'creds.json'), 'r').read()
+j_cred = json.loads(cred)
+user, pwd = [j_cred.username, j_cred.password]
 
 class SearchTool:
     def __init__(self, type):
@@ -8,7 +12,7 @@ class SearchTool:
         pass
 
     def search_all(self):
-        gis = GIS("https://rtaa.maps.arcgis.com", "data_owner", "GIS@RTAA123!")
+        gis = GIS("https://rtaa.maps.arcgis.com", user, pwd)
         search_result = gis.content.search(query="", item_type=self.type)
         return search_result
 
