1d97804fa2394ade61ffdafa1e1fd0d1d3994e8f
AroraTeam <rhughes@aroraengineers.com>
2017-05-02 17:51:15 -0700

    deployment_5_2

--- rtaa_gis/rtaa_gis/settings.py
+++ rtaa_gis/rtaa_gis/settings.py
@@ -212,8 +212,8 @@ DATABASES = {
     #     'PORT': '5432'
     # },
     'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
      }
 }
 
