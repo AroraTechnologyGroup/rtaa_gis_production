27ba0dde70c88ee9d4054228e8c1b71ed69e9191
Richard Hughes <richardh522@gmail.com>
2017-06-08 08:13:32 -0400

    creating admin dashboard

--- rtaa_gis/home/urls.py
+++ rtaa_gis/home/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     url(r'^login/$', login, {'extra_context': {'next': '/#home'}}, name='login'),
     url(r'^logout/$', logout, {'next_page': 'home:login'}, name='logout'),
     url(r'^clear/$', clear_users),
+    url(r'^gis-admin/$', HomePage.as_view(template='home/gisAdmin.html'), name='gis-admin'),
     url(r'^viewer/$', HomePage.as_view(template='home/iframeLoader.html', app_name='rtaa_viewer'), name='viewer'),
     url(r'^eDoc/$', HomePage.as_view(template='home/iframeLoader.html', app_name='eDoc'), name='eDoc'),
     url(r'^airspace/$', HomePage.as_view(template='home/iframeLoader.html', app_name='rtaa_airspace'), name='airspace'),
