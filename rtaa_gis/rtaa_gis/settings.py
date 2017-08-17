3581a16b9456416249cf7a222bad6850b16d5e6d
Richard Hughes <richardh522@gmail.com>
2017-08-17 16:20:29 -0400

    pre-deployment of updates 8_17

--- rtaa_gis/rtaa_gis/settings.py
+++ rtaa_gis/rtaa_gis/settings.py
@@ -19,7 +19,7 @@ DEBUG = True
 # USE_X_FORWARDED_HOST = True
 FORCE_SCRIPT_NAME = "/rtaa_prod/"
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-PYTHON_PATH = r"C:\inetpub\Anaconda3\envs\rtaa_gis\python.exe"
+PYTHON_PATH = r"C:\Program Files (x86)\Anaconda3\envs\rtaa_gis\python.exe"
 LDAP_URL = "gisapps.aroraengineers.com"
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 FILE_APP_TOP_DIRS = [r"\\renofs2\groups\Engineering\Drawings\Std", r"\\renofs2\groups\Engineering\Drawings\Rno"]
@@ -124,7 +124,8 @@ INSTALLED_APPS = [
     'home.apps.HomeConfig',
     'fileApp.apps.FileAppConfig',
     'cloudSync.apps.CloudsyncConfig',
-    'printTool.apps.PrinttoolConfig'
+    'printTool.apps.PrinttoolConfig',
+    'analytics.apps.AnalyticsConfig'
 ]
 
 AUTHENTICATION_BACKENDS = (
