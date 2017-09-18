ce86e32f26ca3045a7b366fcb1464df89dbc8d4c
Richard Hughes <richardh522@gmail.com>
2017-09-18 09:22:39 -0400

    all tests working, pulled fixtures from RTAA

--- rtaa_gis/tests/test_settings.py
+++ rtaa_gis/tests/test_settings.py
@@ -1,6 +1,7 @@
 import os
 SECRET_KEY = 'fake-key'
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+FILE_APP_TOP_DIRS = [r"\\renofs2\groups\Engineering\Drawings\Std", r"\\renofs2\groups\Engineering\Drawings\Rno"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
