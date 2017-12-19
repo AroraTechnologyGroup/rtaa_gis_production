34ebde98b7bc32f9f545508f4bfa303b8a4c9bc3
Richard Hughes <richardh522@gmail.com>
2017-12-19 05:33:57 +0000

    modified settings for staging

--- rtaa_gis/rtaa_gis/settings.py
+++ rtaa_gis/rtaa_gis/settings.py
@@ -8,13 +8,12 @@ import urllib
 import logging
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 # USE_X_FORWARDED_HOST = True
 
 LDAP_URL = "gisapps.aroraengineers.com"
 # LDAP_URL = "renoairport.net"
 
-logging.error(os.path.abspath(__file__).split("\\"))
 if LDAP_URL == "gisapps.aroraengineers.com":
     if "rtaa_gis_django" in os.path.abspath(__file__).split("\\"):
         FORCE_SCRIPT_NAME = "/rtaa_gis/"
@@ -30,8 +29,7 @@ elif LDAP_URL == "renoairport.net":
         FORCE_SCRIPT_NAME = "/applications/"
 
     PYTHON_PATH = r"C:\inetpub\Anaconda3\envs\rtaa_gis\python.exe"
-    # FILE_APP_TOP_DIRS = [r"D:\\", r"\\renofs2\groups\Engineering\Drawings\Std", r"\\renofs2\groups\Engineering\Drawings\Rno"]
-    FILE_APP_TOP_DIRS = [r"C:\\"]
+    FILE_APP_TOP_DIRS = [r"D:\\", r"\\renofs2\groups\Engineering\Drawings\Std", r"\\renofs2\groups\Engineering\Drawings\Rno"]
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ARCPY_PATH = r"C:\Python27\ArcGIS10.5\python.exe"
@@ -302,42 +300,42 @@ LOGGING = {
     'loggers': {
         'analytics': {
             'handlers': ['console', 'file'],
-            'level': "DEBUG",
+            'level': "ERROR",
             'propogate': True
         },
         'diagrams': {
             'handlers': ['console', 'file'],
-            'level': "DEBUG",
+            'level': "ERROR",
             'propogate': True
         },
         'cloudSync': {
             'handlers': ['console', 'file'],
-            'level': "DEBUG",
+            'level': "ERROR",
             'propogate': True
         },
         'lpm': {
             'handlers': ['console', 'file'],
-            'level': "DEBUG",
+            'level': "ERROR",
             'propogate': True
         },
         'fileApp': {
             'handlers': ['console', 'file'],
-            'level': "DEBUG",
+            'level': "ERROR",
             'propogate': True
         },
         'printTool': {
             'handlers': ['console', 'file'],
-            'level': 'DEBUG',
+            'level': "ERROR",
             'propogate': True
         },
         'home': {
             'handlers': ['console', 'file'],
-            'level': "DEBUG",
+            'level': "ERROR",
             'propogate': True
         },
         'django': {
             'handlers': ['console', 'file'],
-            'level': "DEBUG"
+            'level': "ERROR"
         },
         'django.security.DisallowedHost': {
             'handlers': ['file'],
