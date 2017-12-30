94a82b71b05a681c045661824f4b0cb05cc53844
AroraTeam <rhughes@aroraengineers.com>
2017-12-30 14:42:33 -0800

    db

--- rtaa_gis/rtaa_gis/settings.py
+++ rtaa_gis/rtaa_gis/settings.py
@@ -205,16 +205,16 @@ WSGI_APPLICATION = 'rtaa_gis.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'sql_server.pyodbc',
-    #     'NAME': 'GIS_Web',
-    #     'HOST': 'gis.renoairport.net',
-    #     'USER': 'gis',
-    #     'PASSWORD': "GIS@RTAA123!",
-    #     'OPTIONS': {
-    #         'driver': 'SQL Server Native Client 11.0'
-    #      }
-    #  },
+    'default': {
+        'ENGINE': 'sql_server.pyodbc',
+        'NAME': 'GIS_Web',
+        'HOST': 'gis.renoairport.net',
+        'USER': 'gis',
+        'PASSWORD': "GIS@RTAA123!",
+        'OPTIONS': {
+            'driver': 'SQL Server Native Client 11.0'
+         }
+     },
     # 'postGres': {
     #     'ENGINE': 'django.db.backends.postgresql',
     #     'NAME': 'rtaa_DRF',
@@ -223,10 +223,10 @@ DATABASES = {
     #     'HOST': '127.0.0.1',
     #     'PORT': '5432'
     # },
-    'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-     }
+    # 'default': {
+    #      'ENGINE': 'django.db.backends.sqlite3',
+    #      'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #  }
 }
 
 
