ee018dd89640ab8c2034b6224165f17b7d6fd979
Richard Hughes <richardh522@gmail.com>
2018-02-28 17:23:40 -0500

    adding javascript testing

---
+++ gruntfile.js
@@ -0,0 +1,89 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    intern: {
+      options: {
+        excludeInstrumentation: true,
+        suites: ["tests/unit/fileApp_unit.js"],
+        reporters: ['runner']
+      },
+      browser: {
+        options: {
+          environments: 'chrome',
+          loader: {
+            script: "dojo",
+            config: {
+              packages: [
+                { name: "dojo", location: "node_modules/dojo" }
+              ]
+            }
+          }
+        }
+      }
+    },
+    watch: {
+      configFiles: {
+          files: ['gruntfile.js'],
+          options: {
+            'reload': true
+          }
+      },
+      main: {
+        cwd: '.',
+        files: [
+          'rtaa_gis/fileApp/static/**/*.js',
+        ],
+        tasks: [
+          'jshint'
+        ],
+        options: {
+          'spawn': false,
+          'atBegin': true
+        }
+      }
+    },
+    jshint : {
+      options: {
+        reporter: require('jshint-stylish'),
+        curly: true,
+        eqeqeq: true,
+        eqnull: true,
+        browser: true,
+        dojo: true,
+        esversion: 6
+      },
+
+      all: [
+        'gruntfile.js',
+        'rtaa_gis/fileApp/static/fileApp/**/*.js',
+        '!rtaa_gis/fileApp/static/fileApp/pdfjs/**/*.js',
+        'tests/**/*.js'
+      ]
+    },
+    connect: {
+      options: {
+          livereload: false,
+          port: 3003,
+          protocol: 'http'
+      },
+      main: {
+        options: {
+            base: '.',
+            open: {
+                target: 'http://localhost:3003/node_modules/intern/index.html'
+            }
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('intern');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.registerTask('test', ['intern:browser']);
+
+  grunt.registerTask('default', [
+    'jshint',
+    'test'
+  ]);
+};
