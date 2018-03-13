e9ed6d7acaab1e6e70d0c82810eacf6b6bea6bfa
Richard Hughes <richardh522@gmail.com>
2018-03-13 13:02:14 +0000

    adding select-image class

--- rtaa_gis/fileApp/static/fileApp/widgets/App.js
+++ rtaa_gis/fileApp/static/fileApp/widgets/App.js
@@ -390,15 +390,19 @@ define([
 
                     if (should_disable) {
                       domClass.remove(file_icon, "selected-file");
+                      domClass.remove(image_icon, "selected-image");
                     } else {
                       Array.forEach(nodes, function(node) {
                         // disable all other selected files; there currently can be only one selected file
                         domClass.remove(node, "selected-file");
+                        domClass.remove(image_icon, "selected-image");
                       });
                       domClass.add(file_icon, "selected-file");
+                      domClass.add(image_icon, "selected-image");
                     }
                   } else {
                     domClass.add(file_icon, "selected-file");
+                    domClass.add(image_icon, "selected-image");
                   }
 
 
