55b120e8143e908746a9c5cf9e5b94408fe87d9d
Richard Hughes <richardh522@gmail.com>
2018-08-28 20:28:30 +0000

    dev settings

--- rtaa_gis/rtaa_gis/settings.py
+++ rtaa_gis/rtaa_gis/settings.py
@@ -7,7 +7,7 @@ from django.urls import reverse
 import urllib
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 # USE_X_FORWARDED_HOST = True
 
 LDAP_URL = "gisapps.aroraengineers.com"
@@ -219,16 +219,16 @@ WSGI_APPLICATION = 'rtaa_gis.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'sql_server.pyodbc',
-        'NAME': 'GIS_Web',
-        'HOST': 'gis.renoairport.net',
-        'USER': 'gis',
-        'PASSWORD': "GIS@RTAA123!",
-        'OPTIONS': {
-            'driver': 'ODBC Driver 13 for SQL Server'
-         }
-     },
+    # 'default': {
+    #     'ENGINE': 'sql_server.pyodbc',
+    #     'NAME': 'GIS_Web',
+    #     'HOST': 'gis.renoairport.net',
+    #     'USER': 'gis',
+    #     'PASSWORD': "GIS@RTAA123!",
+    #     'OPTIONS': {
+    #         'driver': 'ODBC Driver 13 for SQL Server'
+    #      }
+    #  },
     # 'postGres': {
     #     'ENGINE': 'django.db.backends.postgresql',
     #     'NAME': 'rtaa_DRF',
@@ -237,10 +237,10 @@ DATABASES = {
     #     'HOST': '127.0.0.1',
     #     'PORT': '5432'
     # },
-    # 'default': {
-    #      'ENGINE': 'django.db.backends.sqlite3',
-    #      'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    #  }
+    'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     }
 }
 
 
